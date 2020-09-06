e9a9c63c81d89b65931585b72e3352e5e8fdda4d
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-09-06 14:20:59 -0700

    api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,45 @@
 const router = require("express").Router();
+const {Workout} = require("../models");
 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+  // .limit(1).sort({ $natural: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  // console.log(req.params.id);
+  Workout.findOneAndUpdate({ _id: req.params.id },
+    { $push: { excercises: req.body } },
+    { upsert: true, useFindandModify: false },
+    updatedWorkout => {
+      res.json(updatedWorkout);
+    })
+});
+
+module.exports = router;
